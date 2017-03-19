422da15159469e45db0ba36a1174137d7fa92022
Rene Peuser <rene.peuser@hotmail.de>
2017-03-19 18:54:05 +0100

    update

---
+++ SolutionAndProjects/SolutionAndProjects/ConfigFiles/TcGuiPluginConfigFileService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using SolutionAndProjects.Models;
+using SolutionAndProjects.SpecificFileInfos;
+
+namespace SolutionAndProjects.ConfigFiles
+{
+    public static class TcGuiPluginConfigFileService
+    {
+        private const string ConfigFileExtension = ".config.xml";
+
+        public static IEnumerable<ConfigFileInfo> GetAllConfigFiles(this IEnumerable<ProjectFile> projects)
+        {
+            if (projects == null)
+            {
+                throw new ArgumentNullException(nameof(projects));
+            }
+
+            var configXmlFiles = projects.SelectMany(item => item.ContentItems)
+                                         .Where(item => item.Include.EndsWith(ConfigFileExtension))
+                                         .Select(item => new ConfigFileInfo(item.FileInfo.FullName));
+            return configXmlFiles;
+        }
+    }
+}
---
+++ SolutionAndProjects/SolutionAndProjects/SpecificFileInfos/ConfigFileInfo.cs
@@ -0,0 +1,10 @@
+﻿namespace SolutionAndProjects.SpecificFileInfos
+{
+    public class ConfigFileInfo : SpecificFileInfoBase
+    {
+        public ConfigFileInfo(string path)
+            : base(path, ".xml")
+        {
+        }
+    }
+}
